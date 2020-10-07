bfb72384c39c38b4927b3caaecb20ae269b7b347
Anton Tri Pamungkas <antontripamungkas@gmail.com>
2020-10-07 15:36:44 +0700

    Basic IO

---
+++ app/src/main/java/com/ants/vsga/iofile/MainActivity.java
@@ -0,0 +1,50 @@
+package com.ants.vsga.iofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String FILENAME = "namafile.txt";
+    Button buatFile, ubahFile, bacaFile, deleteFile;
+    TextView textBaca;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buatFile = findViewById(R.id.buttonBuatFile);
+        ubahFile = findViewById(R.id.buttonUbahFile);
+        bacaFile = findViewById(R.id.buttonBacaFile);
+        deleteFile = findViewById(R.id.buttonHapusFile);
+        textBaca = findViewById(R.id.textBaca);
+
+        buatFile.setOnClickListener(this);
+        ubahFile.setOnClickListener(this);
+        bacaFile.setOnClickListener(this);
+        deleteFile.setOnClickListener(this);
+    }
+    void buatFile() {
+        String isiFile = "Coba Isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, true);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
