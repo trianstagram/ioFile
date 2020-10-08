a86a2967a51b39b678bfc359caed8eb63262bd7d
Anton Tri Pamungkas <antontripamungkas@gmail.com>
2020-10-08 14:33:45 +0700

    Basic IO

--- app/src/main/java/com/ants/vsga/iofile/MainActivity.java
+++ app/src/main/java/com/ants/vsga/iofile/MainActivity.java
@@ -47,4 +47,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
---
+++ app/src/main/java/com/ants/vsga/iofile/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.ants.vsga.iofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
